98f44000b2379a8277317ea1e3f20195f6e6631d
lenyas <lenya.luzarenko01@gmail.com>
2022-12-25 21:33:02 +1000

    Initial

---
+++ CMakeFiles/laba3_parallel.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/laba3_parallel.dir/main.cpp.o"
+  "laba3_parallel"
+  "laba3_parallel.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/laba3_parallel.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
